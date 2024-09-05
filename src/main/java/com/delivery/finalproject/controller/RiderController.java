0acdf8d6d00d22d9b87521e70d032e13eecdde93
SoomanKim02 <rlatnaks2@gmail.com>
2024-09-05 14:54:57 +0900

    24_09_05
    2차 merge
    1. dependency 수정
    2. Vo alias

--- src/main/java/com/delivery/finalproject/controller/RiderController.java
+++ src/main/java/com/delivery/finalproject/controller/RiderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.delivery.finalproject.mapper.RiderMapper;
 import com.delivery.finalproject.service.KakaoMapService;

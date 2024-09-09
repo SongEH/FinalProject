da297f1c092a324f31450e423e686d054d01f718
SoomanKim02 <rlatnaks2@gmail.com>
2024-09-09 18:15:06 +0900

    test
    test

--- src/main/java/first/final_project/service/RidersService.java
+++ src/main/java/first/final_project/service/RidersService.java
@@ -49,14 +49,9 @@ public class RidersService {
 
     @Transactional
     public synchronized boolean assignOrderToRider(int orders_id, int raiders_id, String deliveries_method) {
-<<<<<<< Updated upstream
-<<<<<<< HEAD
+
         OrderVo order = riderMapper.getOrderById(orders_id);
-=======
-=======
->>>>>>> Stashed changes
-        OrderVo order = ridersMapper.getOrderById(orders_id);
->>>>>>> riders-branch
+
         if (order == null || !order.getOrders_status().equals("배차 대기")) {
             return false; // 이미 수락된 주문이거나 존재하지 않는 경우
         }
@@ -71,15 +66,9 @@ public class RidersService {
         deliveriesVo.setOrders_id(orders_id);
 
         // 배차 정보 삽입 후, MyBatis가 자동 생성된 deliveries_id를 가져옴
-<<<<<<< Updated upstream
-<<<<<<< HEAD
+
         riderMapper.assignDelivery(deliveriesVo);
-=======
-        ridersMapper.assignDelivery(deliveriesVo);
->>>>>>> riders-branch
-=======
-        ridersMapper.assignDelivery(raiders_id, deliveries_method, orders_id);
->>>>>>> Stashed changes
+
         int deliveries_id = deliveriesVo.getDeliveries_id();
 
         // 배차 이력 생성
@@ -89,23 +78,14 @@ public class RidersService {
     }
 
     public List<OrderVo> getCompletedOrdersByRider(int raiders_id) {
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        return riderMapper.findCompletedOrdersByRider(raiders_id);
-=======
-=======
->>>>>>> Stashed changes
-        return ridersMapper.findCompletedOrdersByRider(raiders_id);
-    }
 
-    @Transactional
-    public void updateDeliveryHistory(int orders_id, String status) {
-        ridersMapper.updateDeliveryHistory(orders_id, status);
->>>>>>> riders-branch
+        return riderMapper.findCompletedOrdersByRider(raiders_id);
     }
 
     @Transactional
     public void updateDeliveryHistory(int orders_id, String status) {
         riderMapper.updateDeliveryHistory(orders_id, status);
+
     }
+
 }

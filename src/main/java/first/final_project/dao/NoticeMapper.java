aa243cfced9866594c6dc37a6fdc4e9a2f77860d
suj5020 <suj5157@icloud.com>
2024-09-12 18:41:12 +0900

    09/12 commit
    공지사항 등록 수정 진행 중

---
+++ src/main/java/first/final_project/dao/NoticeMapper.java
@@ -0,0 +1,21 @@
+package first.final_project.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import first.final_project.vo.NoticeVo;
+
+@Mapper
+public interface NoticeMapper {
+
+    List<NoticeVo> selectListByType(String notice_type);
+
+    NoticeVo selectOneFromIdx(int notice_id);
+
+    int insert(NoticeVo vo);
+
+    int update(NoticeVo vo);
+
+    int delete(int notice_id);
+}

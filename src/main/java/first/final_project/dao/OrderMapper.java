a23e186c12bdba8217d5e032fb43187e4eaacff6
SongEH <will9512@naver.com>
2024-09-13 16:03:39 +0900

    Update acceptOrder

--- src/main/java/first/final_project/dao/OrderMapper.java
+++ src/main/java/first/final_project/dao/OrderMapper.java
@@ -1,8 +1,10 @@
 package first.final_project.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import first.final_project.vo.OrderVo;
 
@@ -17,5 +19,10 @@ public interface OrderMapper {
 
     int delete(int carts_id);
 
+    List<OrderVo> getAcceptOrderList(Map<String, Object> params);
+
+    void updateOrderStatus(@Param("orders_id") int orders_id, @Param("status") String status);
+
+    void deleteOrder(@Param("orders_id") int orders_id);
 
 }

6146445a49713b9d63d39d5668effa7c2d15f670
SoomanKim02 <rlatnaks2@gmail.com>
2024-09-13 18:16:41 +0900

    update Fix Commission

--- src/main/java/first/final_project/util/MyCommon.java
+++ src/main/java/first/final_project/util/MyCommon.java
@@ -7,4 +7,10 @@ public class MyCommon {
 		public static final int BLOCK_LIST = 8; // 한화면에 보여질 게시물수
 		public static final int BLOCK_PAGE = 5; // 한화면에 보여질 페이지수
 	}
+
+	public static class Commission {
+
+		public static final int BLOCK_LIST = 3; // 한화면에 보여질 게시물수
+		public static final int BLOCK_PAGE = 5; // 한화면에 보여질 페이지수
+	}
 }
